5c670b015d6f947f831e3acf6b93f4c686bd2a54
Christian Brunner <31685593+prsbrc@users.noreply.github.com>
2017-09-19 14:09:45 +0200

    Update SND2ME.cmd

--- SND2ME.cmd
+++ SND2ME.cmd
@@ -1,7 +1,7 @@
-             CMD        PROMPT('SND2ME MAIL-VERSAND') TEXT('SND2ME +
-                          MAIL-VERSAND') ALWLMTUSR(*NO) AUT(*EXCLUDE)
+             CMD        PROMPT('SND2ME MAIL-DELIVERY') TEXT('SND2ME +
+                          MAIL-DELIVERY') ALWLMTUSR(*NO) AUT(*EXCLUDE)
              PARM       KWD(OUTQ) TYPE(QUAL1) MIN(1) +
-                          PROMPT('Ausgabewarteschlange')
+                          PROMPT('Outqueue')
  QUAL1:      QUAL       TYPE(*NAME) DFT(SND2ME) MIN(0)
-             QUAL       TYPE(*NAME) DFT(*LIBL) MIN(0) +
-                          PROMPT('Bibliothek')
+             QUAL       TYPE(*NAME) DFT(*LIBL) SPCVAL((*LIBL *LIBL)) +
+                          MIN(0) PROMPT('Library')
