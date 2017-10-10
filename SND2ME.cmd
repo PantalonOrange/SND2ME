f1c90bf289e5da4b763a39717109d25941814740
Christian Brunner <31685593+prsbrc@users.noreply.github.com>
2017-10-10 11:01:19 +0200

    Delete SND2ME.cmd

--- SND2ME.cmd
+++
@@ -1,7 +0,0 @@
-             CMD        PROMPT('SND2ME MAIL-DELIVERY') TEXT('SND2ME +
-                          MAIL-DELIVERY') ALWLMTUSR(*NO) AUT(*EXCLUDE)
-             PARM       KWD(OUTQ) TYPE(QUAL1) MIN(1) +
-                          PROMPT('Outqueue')
- QUAL1:      QUAL       TYPE(*NAME) DFT(SND2ME) MIN(0)
-             QUAL       TYPE(*NAME) DFT(*LIBL) SPCVAL((*LIBL *LIBL)) +
-                          MIN(0) PROMPT('Library')
