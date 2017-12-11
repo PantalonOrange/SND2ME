71790ee541b3da14eb4441c70fcc3670743fe32d
Christian Brunner <31685593+prsbrc@users.noreply.github.com>
2017-12-11 16:20:16 +0100

    New command

---
+++ STRSND2ME.cmd
@@ -0,0 +1,14 @@
+             CMD        PROMPT('START SND2ME MAIL-PDF') TEXT('START +
+                          SND2ME MAIL-PDF') ALLOW(*ALL) MODE(*ALL) +
+                          ALWLMTUSR(*NO) HLPID(*CMD) +
+                          HLPPNLGRP(SND2ME/SND2ME) PRDLIB(SND2ME) +
+                          AUT(*EXCLUDE)
+
+             PARM       KWD(OUTQ) TYPE(QUAL) MIN(1) CHOICE('Valid +
+                          Outqueue') PROMPT('Outqueue')
+             PARM       KWD(TYPE) TYPE(*CHAR) LEN(5) RSTD(*YES) +
+                          DFT(*DTAQ) VALUES(*DTAQ *API) PROMPT('Type')
+
+ QUAL:       QUAL       TYPE(*NAME) DFT(SND2ME) MIN(0)
+             QUAL       TYPE(*NAME) DFT(*LIBL) SPCVAL((*LIBL *LIBL)) +
+                          MIN(0) PROMPT('Library')
